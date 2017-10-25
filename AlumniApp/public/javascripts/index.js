cf10a2db0d3505426c77727e91afa11a12fc99be
Unknown <dsiemer@gmail.com>
2017-10-25 17:26:52 -0500

    Auto stash before merge of "Develop" and "origin/Develop"

---
+++ AlumniApp/public/javascripts/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#Type1").click(function(){
+        $("#Selection1").toggle()
+        
+    });
+    $("#Type2").click(function(){
+        $("#Selection2").toggle();
+    });
+    $("#Type3").click(function(){
+        $("#Selection3").toggle();
+    });
+    $("#Type4").click(function(){
+        $("#Selection4").toggle();
+    });
+    $("#Type5").click(function(){
+        $("#Selection5").toggle();
+    });
+
+});
