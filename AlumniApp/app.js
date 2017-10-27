3f84fd935bb56d7d98faa1b0fb79801f406c7376
rcrutherford <rcrutherford@gmail.com>
2017-10-26 22:07:02 -0500

    with break outs of api files 2

--- AlumniApp/app.js
+++ AlumniApp/app.js
@@ -31,15 +31,6 @@ db.once('open', function() {
 });
 var api = require('./controllers/api.js');
 
-// var ObjectId = require('mongodb').ObjectID;
-// var Schema = mongoose.Schema;
-//alumni schmea was here
-// alumniSchema.plugin(mongoosePaginate);
-
-
-// var Alumni = mongoose.model('Alumni', alumniSchema);
-// var User = mongoose.model('User', userSchema);
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -59,7 +50,6 @@ app.use('/alumniDetails', alumniDetails);
 // app.use('/userform', userform);
 app.use('/getUsers',api.getUsers);
 app.use('/getAlumni',api.getAlumni);
-
 
 
 // catch 404 and forward to error handler
